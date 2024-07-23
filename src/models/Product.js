644eb8afbf2079a5deaccf9c75f075e5b588d8c0
DenisMenendezz <denisuru2017@gmail.com>
2024-07-23 17:35:27 -0300

    Model producto ready

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,12 +1,47 @@
 const { DataTypes } = require("sequelize");
+
 module.exports = (sequelize) => {
-    const Product = sequelize.define('products', {
-      // Define los atributos de tu modelo aquí
+  sequelize.define(
+    "Product",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
       name: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
       },
-      // otros atributos
-    });
-    return Product;
-  };
+      description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      images: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      gender: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      category: { 
+        type: DataTypes.STRING, 
+        allowNull: false,
+      },
+      size: {
+        type: DataTypes.ENUM('S', 'M', 'L', 'XL','XXL'),
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
+};
