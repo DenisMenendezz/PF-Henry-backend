c4e5f2ef74a362e325100fde49a2841ed58a5e0d
DenisMenendezz <denisuru2017@gmail.com>
2024-07-24 18:50:19 -0300

    Creacion de rama development

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const productsRouter = require('./productsRouter');
 
 const router = Router();
 
-// Definición de tus rutas aquí
+// Definición de tus rutas aquí //
 
 router.use("/product", productsRouter)
 
