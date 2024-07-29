d072361c4aec47c570378265cc2981837e11ff75
NahuelArg <claudioarganaraz86@gmail.com>
2024-07-29 20:33:52 -0300

    la tercera es la vencida

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,4 +1,4 @@
-const { Product } = require("../models/Product");
+const { Product } = require("../db");
 const { Op } = require("sequelize");
 
 const getProductsController = async (filters, pagination) => {
